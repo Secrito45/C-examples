0324ccd6ae54cd8b3451e7246cfca41f01740116
Secrito45 <konsta.siekkeli@gmail.com>
2019-03-20 07:49:08 +0200

    My first commit

---
+++ Bitwise Operations/Bitwise1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+char *binbin(int n);
+
+int main()
+{
+    int val = 1;
+    int oring = 0;
+    char byte = 0b01010101;
+
+    printf("Val is %s'B\n", binbin(val));
+
+    for(int i = 0; i < 8; i++)
+    {
+        printf("After shifting %d bit: %s'B\n", i, binbin(val << i));
+    }
+    
+    printf("Val is %s'B\n", binbin(val));
+    printf("\noring is %s'B\n", binbin(oring));
+
+    for(int i = 0; i < 8; i++)
+    {   
+        oring <<= 1;
+        oring |= 0b00000001;
+        printf("After oring %d bit: %s'B\n", i+1, binbin(oring));
+    }
+    printf("\n");
+
+    for(int i = 0; i < 8; i++){
+        if(byte & 0b10000000){
+            printf("1");
+        }
+        else{
+            printf("0");
+        }
+        byte <<= 1;
+    }
+    printf("'B\n");
+
+    return 0;
+}
+
+char *binbin(int n){
+    static char bin[9];
+    int x;
+    for (x = 0; x<8; x++){
+        // Determing is the bit 1 or 0
+        bin[x] = n & 0x80 ? '1' : '0';
+        //Shifting n var's bits one to the right
+        n <<= 1;
+    }
+    bin[x] = 0;
+    return(bin);
+}
---
+++ StructBasics.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+struct names{
+    char firstName[16];
+    char lastName[16];
+};
+
+typedef struct{
+    int val1;
+    int val2;
+}value;
+
+void printStruct(struct names *myStruct)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%s %s\n", myStruct[i].firstName, myStruct[i].lastName);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    //Needs the struct keyword
+    struct names nameList[] = {{"Konsta", "Siekkeli"},
+        {"Aino", "Kauppala"},
+        {"Kaisa", "Siekkeli"},
+        {"Tuulikki", "Haimi-Siekkeli"}
+    };
+    //Don't need the struct keyword
+    value values[3] = {{123, 43},{234,3},{4, 523}};
+
+    printStruct(nameList);
+    /*
+    for(int i = 0; i < 4; i++)
+    {   
+        if(i < 3)
+        {
+            printf("Struct number %d holds: %s %s, %d\n",
+            i+1, nameList[i].firstName, nameList[i].lastName, values[i].val1);
+        }
+        else
+        {
+            printf("This is the final element: %s %s\n",
+             nameList[i].firstName, nameList[i].lastName);
+        }       
+    }
+    */
+    return 0;
+}
